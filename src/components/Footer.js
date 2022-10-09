21d1f9f90ef883cede2d85d1def9ae3402c6de13
LeeviJarvinen <t0jale00@students.oamk.fi>
2022-10-09 15:24:36 +0300

    valmis

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+    <div className='container'>
+        <span>copyright: Leevi Järvinen</span>
+    </div>
+    </footer>
+  )
+}
